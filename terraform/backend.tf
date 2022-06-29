bfeeb6d716ca59e58f0a9461e05cc4daed7a0615
Sven Finke <Sven.Finke@sva.de>
2022-06-29 11:58:11 +0200

    polish

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,9 @@
-# EXAMPLE BACKEND
-# In this PoC build, I will not use a backend as this is not quite 
-# necessary. But DO NOT leave this out when moving on with this.
-# terraform {
-#   backend "s3" {
-#     bucket = "mybucket"
-#     key    = "path/to/my/key"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "remote" {
+    organization = "svenfinke-test"
+ 
+    workspaces {
+      name = "hello_world_cluster"
+    }
+  }
+}
